7729ef30fea88bd7648ff171735a1e479611ef1c
lainso <inazumahp@gmail.com>
2023-06-22 21:54:58 +0900

    lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,29 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+  print(lazypath)
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require('lazy').setup("plugins", opts) -- load lua/lazy-plugins.lua
+
+
+
+-- normal settings
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.textwidth = 0
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.syntax = "on"
+
--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,49 @@ dotfiles=(.zshrc .tmux.conf)
 for file in "${dotfiles[@]}"; do
         ln -svf ~/dotfiles/${file} ~/${file}
 done
+
+
+
+
+
+
+
+
+
+
+
+# Font install
+# シンボリックリンクを作成するフォントディレクトリのパス
+target_font_dir=~/.local/share/fonts
+
+# シンボリックリンクを作成するフォントファイルのディレクトリのパス
+source_dir=.fonts
+
+# シンボリックリンクを作成する関数
+create_font_symlinks() {
+    for file in "$source_dir"/*.ttf; do
+        # ファイル名のみを抽出
+        filename=$(basename "$file")
+        
+        # シンボリックリンクのパスを組み立てる
+        symlink_path="$target_font_dir/$filename"
+        
+        # シンボリックリンクが既に存在する場合はスキップ
+        if [ -e "$symlink_path" ]; then
+            echo "symlink $symlink_path is already exists"
+            continue
+        fi
+        
+        # create symlink
+        ln -s "$file" "$symlink_path"
+        echo "symlink $symlink_path is created"
+    done
+}
+
+# フォントディレクトリが存在しない場合は作成
+if [ ! -d "$target_font_dir" ]; then
+    mkdir -p "$target_font_dir"
+fi
+
+# シンボリックリンクの作成
+create_font_symlinks
